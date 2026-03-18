04edc6292dc476db32a39a2d2f0dd05529d8c6e7
Luis Ferrer Altabás <luisferreraltabas@gmail.com>
2026-03-18 01:50:01 +0100

    Building and checking the graphs

--- DeviceTemplate/states.py
+++ DeviceTemplate/states.py
@@ -2,25 +2,21 @@ from objects import *
 
 # load all states as prestates and save in a dict, then resolve references
 
-states: dict[str,
-    tuple[
-        list[str],
-        list[tuple[TypeAction, tuple[str|int, ...], str]],
-        list[str]]]
+states: states_builder
 
 states = {}
 
-
 resolution = (1000, 1000)
 
+# the first defined state will be the initial state of the circuit
 # id: masks, actions -> id, spontaneous[id]
-states["profile_selected"] = ["mask.png"], [(TypeAction.TAP, (200, 200), "trade")], []
+states["profile_selected"] = ["mask.png"], {TapEllipseAction(Ellipse(540, 1870, 130, 80)): ["waiting_trade"]}
+states["waiting_trade"] = ["mask.png"], {None: ["trade_selection"]}
+states["trade_selection"] = ["mask.png"], {TapEllipseAction(Ellipse(191, 706, 75, 94)): ["first_confirm_trade"]}
+states["first_confirm_trade"] = ["mask.png"], {TapEllipseAction(Ellipse(540, 1906, 300, 70)): ["last_confirm_trade"]}
+states["last_confirm_trade"] = ["mask.png"], {TapEllipseAction(Ellipse(200, 1174, 200, 74)): ["traded_recibed"]}
+states["traded_recibed"] = ["mask.png"], {TapEllipseAction(Ellipse(540, 2131, 50, 50)): ["profile_selected"]}
 
-pre_states: dict[str, PreState]
-pre_states = {}
-for k, v in states.items():
-    actions = {Action(a[0], *a[1]): a[2] for a in v[1]}
-    pre_states[k] = PreState(v[0], actions, v[2], resolution)
 
 # no se pueden resolver las referencias sin tener dentro de una clase ambas opciones como válidas
 # porque aunque haga prestate, no estan definidos todos los states cuando vaya a empezar a definirlos
--- objects.py
+++ objects.py
@@ -85,34 +85,107 @@ class TapAction(Action):
 
 class ButtonAction(Action):
     key_code: int
-    key_alias: str
+    key_alias: Optional[str]
     def __init__(self, key_code: int, key_alias:Optional[str]=None) -> None:
         self.key_code = key_code
-        self.key_alias = str(key_code) if key_alias is None else key_alias
+        self.key_alias = key_alias
     def __str__(self):
         return f"[button] key_code: {self.key_code}"
     def __repr__(self) -> str:
+        if self.key_alias is None:
+            return self.__str__()
         return f"[button] key: {self.key_alias}; key_code: {self.key_code}"
     async def act(self, device: ControllableDeviceAsync) -> None:
         await device.send_key(self.key_code)
         
 
-class PreState:
-    masks: list[png]
-    edges_by_action: dict[Action, str]
-    spontaneous_states: set[str]
-    def __init__(self, masks_strings: list[str], edges: dict[Action, str], spontaneous_states_list: list[str], resolution:tuple[int,int]) -> None:
-        masks = []
-        for m in masks_strings:
-            if m is None:
-                raise ValueError
-            if resolution:
-                raise ValueError
-            masks.append(m)
-        edges_by_action = edges
-        spontaneous_states = set(spontaneous_states_list)
+# class PreState:
+#     masks: list[png]
+#     edges_by_action: dict[Action, str]
+#     spontaneous_states: set[str]
+#     def __init__(self, masks_strings: list[str], edges: dict[Action, str], spontaneous_states_list: list[str], resolution:tuple[int,int]) -> None:
+#         masks = []
+#         for m in masks_strings:
+#             if m is None:
+#                 raise ValueError
+#             if resolution:
+#                 raise ValueError
+#             masks.append(m)
+#         edges_by_action = edges
+#         spontaneous_states = set(spontaneous_states_list)
 
 class State:
-    masks: list[png]
-    edges_by_action: dict[Action, State]
-    spontaneous_states: set[State]
+    masks: Optional[list[str]]
+    # the set[State] assigned to None is the set with the spontaneous states
+    edges_by_action: dict[Optional[Action], set[Self]]
+    def __init__(self, masks) -> None:
+        self.masks = masks
+        self.edges_by_action = {}
+    def is_obvious_state(self) -> bool:
+        return self.masks is None or len(self.masks) == 0
+    def connect(self, action: Optional[Action], edges: set[Self]):
+        self.edges_by_action[action] = edges
+
+states_builder = dict[str,
+                    tuple[list[str],
+                          dict[Optional[Action], list[str]]]]
+
+def load_graph(file: str) -> State:
+    # load states from the file
+    states:states_builder = {}
+
+    nodes:dict[str, State] = {}
+    # 1. Crear nodos
+    for reference, (masks, _) in states.items():
+        if len(masks) != len(set(masks)):
+            # log warning masks dupped in the same state
+            # no se usa set porque el orden es importante
+            pass
+        nodes[reference] = State(
+            masks
+        )
+
+    # 2. Conectar
+    for reference, (_, actions) in states.items():
+        for action, edges in actions.items():
+            set_edges = set(edges)
+            if len(edges) != len(set_edges):
+                # log info edges dupped in the same action, dups are removed
+                pass
+            if len(set_edges) == 0:
+                # nodo sin salida, debe cerrar circuitos
+                pass
+            set_nodes = set(nodes[e] for e in set_edges)
+            if sum(n.is_obvious_state() for n in set_nodes) > 1:
+                raise SyntaxError("An action can't derive in several states unchecked by masks")
+            nodes[reference].connect(action, set_nodes)
+        # dentro de cada set no puede haber más de un state sin máscaras
+
+    # 3. Comprobar nodos
+    # no puede haber referencias a nodos sin resolver, keyerror en paso 2
+
+
+    # 4. Comprobar grafo
+    # si tiene nodos sin retorno deben ser condiciones para salir de todos los circuitos que se hagan sobre este grafo, avisar
+    # no debe haber nodos inalcanzables, no tendrían sentido
+    # por lo mismo dos nodos separados pero unidos entre sí tampoco sirven
+    nodes_that_can_reach_all = set()
+    for reference in states.keys():
+        nodes_visited:set[str] = set()
+        nodes_to_visit:set[str] = set()
+        nodes_to_visit.add(reference)
+        while True:
+            try:
+                edge = nodes_to_visit.pop()
+            except KeyError:
+                break
+            nodes_visited.add(edge)
+            if edge in nodes_that_can_reach_all or len(nodes_visited) == len(nodes):
+                nodes_that_can_reach_all.add(reference)
+                break
+            for edges in states[edge][1].values():
+                for edge in edges:
+                    if edge not in nodes_visited:
+                        nodes_to_visit.add(edge)
+
+    return nodes[next(iter(states.keys()))]
